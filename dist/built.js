303d9fc2caca60969fc3ea528699da0f19778427
unknown <583274568@qq.com>
2016-09-01 11:03:36 +0800

    新增计算地球两点的球面距离

--- dist/built.js
+++ dist/built.js
@@ -45,6 +45,46 @@ function delCookie(name) {
         document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
     }
 }
+var EARTH_RADIUS = 6378137.0; //单位M
+var PI = Math.PI;
+
+function getRad(d){
+    return d*PI/180.0;
+}
+/*获取地球上两点间的*/
+function getFlatternDistance(lat1,lng1,lat2,lng2){
+    lat1=parseFloat(lat1);
+    lng1=parseFloat(lng1);
+    lat2=parseFloat(lat2);
+    lng2=parseFloat(lng2);
+    var f = getRad((lat1 + lat2)/2);
+    var g = getRad((lat1 - lat2)/2);
+    var l = getRad((lng1 - lng2)/2);
+
+    var sg = Math.sin(g);
+    var sl = Math.sin(l);
+    var sf = Math.sin(f);
+
+    var s,c,w,r,d,h1,h2;
+    var a = EARTH_RADIUS;
+    var fl = 1/298.257;
+
+    sg = sg*sg;
+    sl = sl*sl;
+    sf = sf*sf;
+
+    s = sg*(1-sl) + (1-sf)*sl;
+    c = (1-sg)*(1-sl) + sf*sl;
+
+    w = Math.atan(Math.sqrt(s/c));
+    r = Math.sqrt(s*c)/w;
+    d = 2*w*a;
+    h1 = (3*r -1)/2/c;
+    h2 = (3*r +1)/2/s;
+
+    return parseInt(d*(1 + fl*(h1*sf*(1-sg) - h2*(1-sf)*sg))/1000);
+    //返回km
+}
 /**
  * 在微信中进行导航，如果失败则调用腾讯地图api
  */
